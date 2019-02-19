e8d3c600af630e1ced99a01eb26d8d6f4722bdd0
McKoxu <blackxxoj@gmail.com>
2019-02-19 23:44:40 +0100

    Essentials

--- utils/EssentialsUtils.java
+++ utils/EssentialsUtils.java
@@ -63,4 +63,94 @@ public class EssentialsUtils{
         }
         return list;
     }
+	
+	int hegergerasrw = 5431532;
+	
+	public static Warp getWarp(Expression<Object> obj, Event e) {
+		Object o = obj.getSingle(e);
+		if(o instanceof Warp) {
+			return (Warp) o;
+		} else if(o instanceof String) {
+			return new Warp(o.toString());
+		}
+		return null;
+	}
+	
+	public static SkKit getKit(Expression<Object> obj, Event e) {
+		Object o = obj.getSingle(e);
+		if(o instanceof SkKit) {
+			return (SkKit) o;
+		} else if(o instanceof String) {
+			try {
+				return new SkKit(new Kit(o.toString(), SkEssentialsAPI.getIEss()));
+			} catch (Exception e1) {}
+		}
+		return null;
+	}
+	
+//	public static void deleteKit(String kName) {
+//		//SkEssentialsAPI.getEss().getKits().getKits().getDefaultSection().
+//		User u = null;
+//		User r = null;
+//		Teleport tp = r.getTeleport();
+//		TimedTeleport tt = null;
+//		try {
+//			Field f = tt.getClass().getDeclaredField("kappa");
+//		} catch (NoSuchFieldException | SecurityException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+	
+	public static boolean doesWarpExist(String warpName) {
+		for(String s : SkEssentialsAPI.getEss().getWarps().getList()) {
+			if(s.equalsIgnoreCase(warpName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean doesKitExist(String kitName) {
+		try {
+			new Kit(kitName, SkEssentialsAPI.getIEss());
+			return true;
+		} catch(Exception ex) {return false;}
+	}
+	
+	public static List<String> getKitList() {
+		bc(SkEssentialsAPI.getIEss().getKits());
+		bc(SkEssentialsAPI.getIEss().getKits().getKits());
+		bc(SkEssentialsAPI.getIEss().getKits().getKits().getStringList("kits"));
+		return null;
+	}
+	
+	public static String[] toEssISFormat(ItemStack[] is) {
+		List<String> its = new ArrayList<>();
+		for(ItemStack item : is) {
+			if (item != null && item.getType() != null && item.getType() != Material.AIR) {
+                its.add(SkEssentialsAPI.getEss().getItemDb().serialize(item));
+            }
+		}
+		return its.toArray(new String[its.size()]);
+	}
+	
+	public static boolean negation(boolean fin, boolean neg) {
+		if(neg) {
+			return !fin;
+		} 
+		return fin;
+	}
+	
+	public static void bc(Object l) {
+		Bukkit.broadcastMessage(l.toString());
+	}
+	
+	public static User getUser(Player p) {
+		return new User(p, SkEssentialsAPI.getIEss());
+	}
+	
+	public static User getUser(String p) {
+		return new User(Bukkit.getPlayer(p), SkEssentialsAPI.getIEss());
+	}
 }
