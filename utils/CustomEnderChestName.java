0565b914ffd1203b3684e5727c3b227a09a4dd1f
McKoxu <blackxxoj@gmail.com>
2019-04-04 22:18:06 +0200

    Create CustomEnderChestName.java

---
+++ utils/CustomEnderChestName.java
@@ -0,0 +1,10 @@
+try {
+    Field a = EntityHuman.class.getDeclaredField("enderChest");
+    a.setAccessible(true);
+    inventoryEnderChest enderChest = (InventoryEnderChest) a.get((EntityHuman) ((CraftPlayer) player).getHandle());
+    Field chestA = InventorySubcontainer.class.getDeclaredField("a");
+    chestA.setAccessible(true);
+    chestA.set(enderChest, ChatUtil.color("&6Ender Chest Kek"));
+} catch(Exception ex){
+    ex.printStackTrace();
+}
