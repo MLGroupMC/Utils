c62ff082e45055bbde275e1088c8d4a5f4794c98
McKoxu <blackxxoj@gmail.com>
2019-12-08 10:18:41 +0100

    Alphabetize sort if values are equal

--- utils/ComparatorSort.java
+++ utils/ComparatorSort.java
@@ -5,23 +5,38 @@ import java.util.*;
 public class StandardSort {
   
     /*
-      Sortowanie malejące (według wartości User::getLvl)
+      Sortowanie malejące (według wartości User::getLevel)
     */
     public static List<User> sort(List<User> toSort) {
         TreeSet<User> treeSet = new TreeSet<>(new ValueComparator());
         treeSet.addAll(toSort);
         return new ArrayList<>(treeSet);
     }
+	
 
     private static class ValueComparator implements Comparator<User> {
 
-        ValueComparator() {}
+		ValueComparator() {}
 
-        @Override
+		@Override
         public int compare(User a, User b) {
-            return a.getLvl() < b.getLvl() ? 1 : -1;
-        }
+            if(a.getLevel() < b.getLevel()){
+                return 1;
+            } else{
+                if(a.getLevel() == b.getLevel()){
+                    int compare = a.getName().compareTo(b.getName());
 
+                    if (compare < 0 || compare == 0) {
+                        return -1;
+                    } else if (compare > 0) {
+                        return 1;
+                    }
+                } else{
+                    return -1;
+                }
+                return -1;
+            }
+        }
     }
 
 }
